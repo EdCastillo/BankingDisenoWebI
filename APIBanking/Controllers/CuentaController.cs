53b0322bce93153eff2943b5ae3df859b8b15383
AshredCR <55962947+EdCastillo@users.noreply.github.com>
2021-06-02 20:06:37 -0600

    Class update

---
+++ APIBanking/Controllers/CuentaController.cs
@@ -0,0 +1,166 @@
+﻿using APIBanking.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace APIBanking.Controllers
+{
+    [AllowAnonymous]
+    [RoutePrefix("api/cuenta")]
+    public class CuentaController : ApiController
+    {
+
+        [HttpGet]
+        public IHttpActionResult getByID(int id) {
+            if (id == 0) {
+                return BadRequest();
+            }
+            try
+            {
+                using (SqlConnection sqlConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["Banking"].ConnectionString))
+                {
+                    SqlCommand sqlCommand = new SqlCommand(@"SELECT Codigo,CodigoUsuario,Descripcion,IBAN,CodigoMoneda,Saldo,Estado FROM Cuenta where Codigo=@Codigo", sqlConnection);
+                    sqlCommand.Parameters.AddWithValue("@Codigo", id);
+                    Cuenta cuenta = new Cuenta();
+                    sqlConnection.Open();
+                    SqlDataReader reader = sqlCommand.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        cuenta = new Cuenta { Codigo = reader.GetInt32(0), CodigoUsuario = reader.GetInt32(1), Descripcion = reader.GetString(2), IBAN = reader.GetString(3), CodigoMoneda = reader.GetInt32(4), Saldo = reader.GetDecimal(5), Estado = reader.GetString(6) };
+                    }
+                    sqlConnection.Close();
+                    return Ok(cuenta);
+                }
+            }
+            catch (Exception ex)
+            {
+
+                return InternalServerError(ex);
+            }
+        }
+        [HttpGet]
+        public IHttpActionResult getAll()
+        {
+            try
+            {
+                using (SqlConnection sqlConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["Banking"].ConnectionString))
+                {
+                    SqlCommand sqlCommand = new SqlCommand(@"SELECT Codigo,CodigoUsuario,Descripcion,IBAN,CodigoMoneda,Saldo,Estado FROM Cuenta", sqlConnection);
+                    List<Cuenta> list = new List<Cuenta>();
+                    sqlConnection.Open();
+                    SqlDataReader reader = sqlCommand.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        list.Add(new Cuenta { Codigo = reader.GetInt32(0), CodigoUsuario = reader.GetInt32(1), Descripcion = reader.GetString(2), IBAN = reader.GetString(3), CodigoMoneda = reader.GetInt32(4), Saldo = reader.GetDecimal(5), Estado = reader.GetString(6) });
+                    }
+                    sqlConnection.Close();
+                    return Ok(list);
+                }
+            }
+            catch (Exception ex)
+            {
+
+                return InternalServerError(ex);
+            }
+        }
+
+
+        [HttpPost]
+        public IHttpActionResult Ingresar(Cuenta cuenta) {
+            if (cuenta == null)
+                return BadRequest();
+            try
+            {
+                using (SqlConnection sqlConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["Banking"].ConnectionString))
+                {
+                    SqlCommand sqlCommand = new SqlCommand(@"insert into Cuenta output inserter.Codigo values(@CodigoUsuario,@Descripcion,@IBAN,@CodigoMoneda,@Saldo,@Estado)", sqlConnection);
+                    sqlCommand.Parameters.AddWithValue("@CodigoUsuario",cuenta.CodigoUsuario);
+                    sqlCommand.Parameters.AddWithValue("@Descripcion", cuenta.Descripcion);
+                    sqlCommand.Parameters.AddWithValue("@IBAN", cuenta.IBAN);
+                    sqlCommand.Parameters.AddWithValue("@CodigoMoneda", cuenta.CodigoMoneda);
+                    sqlCommand.Parameters.AddWithValue("@Saldo", cuenta.Saldo);
+                    sqlCommand.Parameters.AddWithValue("@Estado", cuenta.Estado);
+                    sqlConnection.Open();
+                    SqlDataReader reader = sqlCommand.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        cuenta.Codigo = reader.GetInt32(0);
+                    }
+                    sqlConnection.Close();
+                    return Ok(cuenta);
+                }
+            }
+            catch (Exception ex)
+            {
+
+                return InternalServerError(ex);
+            }
+        }
+
+
+        [HttpPut]
+        public IHttpActionResult Actualizar(Cuenta cuenta)
+        {
+            if (cuenta == null)
+                return BadRequest();
+            try
+            {
+                using (SqlConnection sqlConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["Banking"].ConnectionString))
+                {
+                    SqlCommand sqlCommand = new SqlCommand(@"update Cuenta set CodigoUsuario=@CodigoUsuario,Decripcion=@Descripcion,IBAN=@IBAN,CodigoMoneda=@CodigoMoneda,Saldo=@Saldo,Estado=@Estado where Codigo=@Codigo", sqlConnection);
+                    sqlCommand.Parameters.AddWithValue("@Codigo", cuenta.Codigo);
+                    sqlCommand.Parameters.AddWithValue("@CodigoUsuario", cuenta.CodigoUsuario);
+                    sqlCommand.Parameters.AddWithValue("@Descripcion", cuenta.Descripcion);
+                    sqlCommand.Parameters.AddWithValue("@IBAN", cuenta.IBAN);
+                    sqlCommand.Parameters.AddWithValue("@CodigoMoneda", cuenta.CodigoMoneda);
+                    sqlCommand.Parameters.AddWithValue("@Saldo", cuenta.Saldo);
+                    sqlCommand.Parameters.AddWithValue("@Estado", cuenta.Estado);
+                    sqlConnection.Open();
+                    int filasAfectadas = sqlCommand.ExecuteNonQuery();
+                    sqlConnection.Close();
+                    if (filasAfectadas > 0) {
+                        return Ok(cuenta);
+                    }
+                    return BadRequest();
+                }
+            }
+            catch (Exception ex)
+            {
+
+                return InternalServerError(ex);
+            }
+        }
+        [HttpDelete]
+        public IHttpActionResult Borrar(int id)
+        {
+            if (id == 0)
+                return BadRequest();
+            try
+            {
+                using (SqlConnection sqlConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["Banking"].ConnectionString))
+                {
+                    SqlCommand sqlCommand = new SqlCommand(@"delete Cuenta  where Codigo=@Codigo", sqlConnection);
+                    sqlCommand.Parameters.AddWithValue("@Codigo", id);
+                    sqlConnection.Open();
+                    int filasAfectadas = sqlCommand.ExecuteNonQuery();
+                    sqlConnection.Close();
+                    if (filasAfectadas > 0)
+                    {
+                        return Ok();
+                    }
+                    return BadRequest();
+                }
+            }
+            catch (Exception ex)
+            {
+
+                return InternalServerError(ex);
+            }
+        }
+    }
+}
--- APIBanking/Controllers/LoginController.cs
+++ APIBanking/Controllers/LoginController.cs
@@ -27,6 +27,7 @@ namespace APIBanking.Controllers
                 Usuario user = returnUserOnValidation(login);
                 if (user.Codigo!=0)
                 {
+                    user.Password = null;
                     user.TOKEN = TokenGenerator.GenerateTokenJwt(login.Username);
                     return Ok(user);
                 }
@@ -43,18 +44,67 @@ namespace APIBanking.Controllers
         }
         private Usuario returnUserOnValidation(LoginRequest login) {
             using (SqlConnection sqlConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["Banking"].ConnectionString)) {
-                SqlCommand sqlCommand = new SqlCommand(@"SELECT Codigo,Username,Email,Estado,Nombre FROM USUARIO WHERE Username=@USERNAME AND Password=@PASSWORD", sqlConnection);
+                SqlCommand sqlCommand = new SqlCommand(@"SELECT Codigo,Username,Email,Estado,Nombre,Identificacion,FechaNacimiento FROM USUARIO WHERE Username=@USERNAME AND Password=@PASSWORD", sqlConnection);
                 sqlCommand.Parameters.AddWithValue("@USERNAME", login.Username);
                 sqlCommand.Parameters.AddWithValue("@PASSWORD", login.Password);
                 sqlConnection.Open();
                 Usuario usuario = new Usuario();
                 SqlDataReader reader = sqlCommand.ExecuteReader();
                 while (reader.Read()) {
-                    usuario = new Usuario { Codigo = reader.GetInt32(0), Username = reader.GetString(1),Email=reader.GetString(2),Estado=reader.GetString(3),Nombre=reader.GetString(4) };
+                    usuario = new Usuario { Codigo = reader.GetInt32(0), Username = reader.GetString(1),Email=reader.GetString(2),Estado=reader.GetString(3),Nombre=reader.GetString(4),Identificacion=reader.GetString(5),FechaNacimiento=reader.GetDateTime(6) };
                 }
                 sqlConnection.Close();
                 return usuario;
             }
         }
+        [HttpPost]
+        [Route("ingresar")]
+        public IHttpActionResult registrar(Usuario usuario) {
+            if (usuario == null) {
+                return BadRequest();
+            }
+            try {
+                Usuario newUser = privateInsert(usuario);
+                if (newUser.Codigo == 0)
+                {
+                    return InternalServerError();
+                }
+                else {
+                    newUser.TOKEN = TokenGenerator.GenerateTokenJwt(newUser.Username);
+                    return Ok(newUser);
+                }
+
+            }
+            catch (Exception ex) {
+                return InternalServerError(ex);
+            }
+        }
+
+
+        private Usuario privateInsert(Usuario usuario) {
+            using (SqlConnection sqlConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["Banking"].ConnectionString)) {
+                SqlCommand sqlCommand = new SqlCommand(@"INSERT INTO USUARIO OUTPUT inserted.Codigo VALUES(@Identificacion,@Nombre,@Username,@Password,@Email,@FechaNacimiento,@Estado)",sqlConnection);
+                sqlCommand.Parameters.AddWithValue("@Identificacion", usuario.Identificacion);
+                sqlCommand.Parameters.AddWithValue("@Nombre", usuario.Nombre);
+                sqlCommand.Parameters.AddWithValue("@Username", usuario.Username);
+                sqlCommand.Parameters.AddWithValue("@Password", usuario.Password);
+                sqlCommand.Parameters.AddWithValue("@Email", usuario.Email);
+                sqlCommand.Parameters.AddWithValue("@FechaNacimiento", usuario.FechaNacimiento);
+                sqlCommand.Parameters.AddWithValue("@Estado", usuario.Estado);
+                sqlConnection.Open();
+                SqlDataReader reader = sqlCommand.ExecuteReader();
+                while (reader.Read()) {
+                    usuario.Codigo = reader.GetInt32(0);
+                }
+                sqlConnection.Close();
+                return usuario;
+            }
+        }
+
     }
+    
+
+
+
+
 }
