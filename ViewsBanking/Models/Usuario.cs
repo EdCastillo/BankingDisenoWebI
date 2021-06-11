6107718d15895d8d6417863900529ba40a23735f
AshredCR <55962947+EdCastillo@users.noreply.github.com>
2021-06-11 11:25:14 -0600

    Total manager code

---
+++ ViewsBanking/Models/Usuario.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ViewsBanking.Models
+{
+    public class Usuario
+    {
+        public Usuario()
+        {
+            this.Cuenta = new HashSet<Cuenta>();
+            this.Error = new HashSet<Error>();
+            this.Estadistica = new HashSet<Estadistica>();
+            this.Sesion = new HashSet<Sesion>();
+        }
+
+        public int Codigo { get; set; }
+        public string Identificacion { get; set; }
+        public string Nombre { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public System.DateTime FechaNacimiento { get; set; }
+        public string Estado { get; set; }
+        public string TOKEN { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<Cuenta> Cuenta { get; set; }
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<Error> Error { get; set; }
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<Estadistica> Estadistica { get; set; }
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<Sesion> Sesion { get; set; }
+    }
+}
