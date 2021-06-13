783453866d00cbf4ae48897251848adbdd670dc7
AshredCR <55962947+EdCastillo@users.noreply.github.com>
2021-06-13 17:41:13 -0600

    Staged

---
+++ ViewsBanking/Controllers/UsuarioController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using ViewsBanking.Managers;
+using ViewsBanking.Models;
+
+namespace ViewsBanking.Controllers
+{
+    public  class UsuarioController : Controller
+    {
+        
+        public ActionResult Login()
+        {
+            return View();
+        }
+        public async Task<ActionResult> Authenticate(string username,string password) {
+            UsuarioManager manager = new UsuarioManager();
+            //Usuario user=await manager.Insertar(new Usuario {Identificacion="asd",Nombre="asd",Username="assd",Password="asd",Email="asd",FechaNacimiento=DateTime.Now,Estado="a" });
+            Usuario usuario=await manager.Login(new LoginRequest { Username = username, Password = password });
+            if (usuario.Codigo == 0) {
+
+                Redirect("");
+            }
+            return View(usuario);
+        }
+    }
+}
---
+++ ViewsBanking/Models/Moneda.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ViewsBanking.Models
+{
+    public partial class Moneda
+    {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+        public Moneda()
+        {
+            this.Cuenta = new HashSet<Cuenta>();
+            this.Pago = new HashSet<Pago>();
+        }
+
+        public int Codigo { get; set; }
+        public string Descripcion { get; set; }
+        public string Estado { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<Cuenta> Cuenta { get; set; }
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<Pago> Pago { get; set; }
+    }
+}
