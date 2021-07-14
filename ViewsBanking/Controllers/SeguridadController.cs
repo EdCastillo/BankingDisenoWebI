32952f6e61d7c90511623f33296cd58960898407
Kevin <kevcarmora@gmail.com>
2021-07-13 22:38:02 -0600

    Prueba

---
+++ ViewsBanking/Controllers/SeguridadController.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using ViewsBanking.Managers;
+using ViewsBanking.Models;
+
+namespace ViewsBanking.Controllers
+{
+    public class SeguridadController : Controller
+    {
+        // GET: Seguridad
+        public async Task<ActionResult> Index(string token)
+        {
+
+            SeguridadManager manager = new SeguridadManager();
+            IEnumerable<Seguridad> list = await manager.GetAll(token);
+            return View(list);
+        }
+
+        // GET: Seguridad/Details/5
+        public async Task<ActionResult> Details(int id, string token)
+        {
+            SeguridadManager manager = new SeguridadManager();
+            Seguridad seguridad = await manager.GetByID(id, token);
+            return View(seguridad);
+
+        }
+
+        // GET: Seguridad/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Seguridad/Create
+        [HttpPost]
+        public async Task<ActionResult> Create(Seguridad seguridad, string token)
+        {
+            try
+            {
+                SeguridadManager manager = new SeguridadManager();
+                await manager.Insertar(seguridad, token);
+                return RedirectToAction("Index", new { token = token });
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Seguridad/Edit/5
+        public async Task<ActionResult> Edit(int id, string token)
+        {
+            SeguridadManager manager = new SeguridadManager();
+            Seguridad seguridad = await manager.GetByID(id, token);
+            return View(seguridad);
+        }
+
+
+
+        // POST: Seguridad/Edit/5
+        [HttpPost]
+        public async Task<ActionResult> Edit(Seguridad seguridad, string token)
+        {
+            try
+            {
+                SeguridadManager manager = new SeguridadManager();
+                await manager.Actualizar(seguridad, token);
+                return RedirectToAction("Index", new { token = token });
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+
+
+        // POST: Seguridad/Delete/5
+        public async Task<ActionResult> Delete(int id, string token)
+        {
+            // TODO: Add delete logic here
+            SeguridadManager manager = new SeguridadManager();
+            await manager.Eliminar(id, token);
+            return RedirectToAction("Index", new { token = token });
+        }
+    }
+}
