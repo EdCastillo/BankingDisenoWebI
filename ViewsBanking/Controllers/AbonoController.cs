32b65b245c429fd28039345c00fc434038d759b1
AshredCR <55962947+EdCastillo@users.noreply.github.com>
2021-08-16 12:02:55 -0600

    Abono last push

--- ViewsBanking/Controllers/AbonoController.cs
+++ ViewsBanking/Controllers/AbonoController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Dynamic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Web;
@@ -12,16 +13,23 @@ namespace ViewsBanking.Controllers
     public class AbonoController : Controller
     {
         // GET: Abono
-        public async Task<ActionResult> Index(string token)
+        public async Task<ActionResult> Index()
         {
+            string token=Session["Token"].ToString();
             AbonoManager manager = new AbonoManager();
+            PrestamoManager prestamoManager = new PrestamoManager();
+            IEnumerable<Prestamo> prestamos = await prestamoManager.GetAll(token);
             IEnumerable<Abono> list = await manager.GetAll(token);
-            return View(list);
+            dynamic model = new ExpandoObject();
+            model.Abono = list;
+            model.Prestamo = prestamos;
+            return View(model);
         }
 
         // GET: Abono/Details/5
-        public async Task<ActionResult> Details(int id,string token)
+        public async Task<ActionResult> Details(int id)
         {
+            string token = Session["Token"].ToString();
             AbonoManager manager = new AbonoManager();
             Abono abono = await manager.GetByID(id,token);
             return View(abono);
@@ -35,16 +43,18 @@ namespace ViewsBanking.Controllers
 
         // POST: Abono/Create
         [HttpPost]
-        public async Task<ActionResult> Create(Abono abono,string token)
+        public async Task<ActionResult> Create(Abono abono)
         {
-                AbonoManager manager = new AbonoManager();
+            string token = Session["Token"].ToString();
+            AbonoManager manager = new AbonoManager();
                 await manager.Insertar(abono, token);
                 return RedirectToAction("Index",new { token=token});
         }
 
         // GET: Abono/Edit/5
-        public async Task<ActionResult> Edit(int id,string token)
+        public async Task<ActionResult> Edit(int id)
         {
+            string token = Session["Token"].ToString();
             AbonoManager manager = new AbonoManager();
             Abono abono = await manager.GetByID(id, token);
             return View(abono);
@@ -52,16 +62,18 @@ namespace ViewsBanking.Controllers
 
         // POST: Abono/Edit/5
         [HttpPost]
-        public async Task<ActionResult> Edit(Abono abono,string token)
+        public async Task<ActionResult> Edit(Abono abono)
         {
+            string token = Session["Token"].ToString();
             AbonoManager manager = new AbonoManager();
             await manager.Actualizar(abono,token);
             return RedirectToAction("Details",new {id=abono.Codigo,token=token });
         }
 
         // GET: Abono/Delete/5
-        public async Task<ActionResult> Delete(int id,string token)
+        public async Task<ActionResult> Delete(int id)
         {
+            string token = Session["Token"].ToString();
             AbonoManager manager = new AbonoManager();
             await manager.Eliminar(id,token);
             return RedirectToAction("Index",new{token=token});
