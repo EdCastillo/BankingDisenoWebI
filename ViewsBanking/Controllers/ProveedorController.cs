22c583ca3d32af18d48f79e1bfbac59ffee4457c
Vlaxtor <44406381+Vlaxtor@users.noreply.github.com>
2021-08-17 00:33:32 -0600

    Proveedor last update

--- ViewsBanking/Controllers/ProveedorController.cs
+++ ViewsBanking/Controllers/ProveedorController.cs
@@ -11,17 +11,26 @@ namespace ViewsBanking.Controllers
 {
     public class ProveedorController : Controller
     {
+     private static ProveedorManager manager=new ProveedorManager();
         // GET: Proveedor
-        public async Task<ActionResult> Index(string token)
+        public async Task<ActionResult> Index()
         {
+         string token = Session["Token"].ToString();
             ProveedorManager manager = new ProveedorManager();
             IEnumerable<Proveedor> list = await manager.GetAll(token);
-            return View(list);
+            ServicioManager managerPrest = new ServicioManager();
+            IEnumerable<Servicio> listServicio = await managerServ.GetAll(token);
+            
+                  dynamic model = new ExpandoObject();
+            model.Proveedor = list;
+             model.Servicio = listServicio;
+            return View(model);
         }
 
         // GET: Proveedor/Details/5
-        public async Task<ActionResult> Details(int id, string token)
+        public async Task<ActionResult> Details(int id)
         {
+         string token = Session["Token"].ToString();
             ProveedorManager manager = new ProveedorManager();
             Proveedor proveedor = await manager.GetByID(id, token);
             return View(proveedor);
@@ -35,17 +44,18 @@ namespace ViewsBanking.Controllers
 
         // POST: Proveedor/Create
         [HttpPost]
-        public async Task<ActionResult> Create(Proveedor proveedor, string token)
+        public async Task<ActionResult> Create(Proveedor proveedor)
         {
-
+ string token = Session["Token"].ToString();
             ProveedorManager manager = new ProveedorManager();
             await manager.Insertar(proveedor, token);
             return RedirectToAction("Index", new { token = token });
         }
 
         // GET: Proveedor/Edit/5
-        public async Task<ActionResult> Edit(int id, string token)
+        public async Task<ActionResult> Edit(int id)
         {
+         string token = Session["Token"].ToString();
             ProveedorManager manager = new ProveedorManager();
             Proveedor proveedor = await manager.GetByID(id, token);
             return View(proveedor);
@@ -53,16 +63,18 @@ namespace ViewsBanking.Controllers
 
         // POST: Proveedor/Edit/5
         [HttpPost]
-        public async Task<ActionResult> Edit(Proveedor proveedor, string token)
+        public async Task<ActionResult> Edit(Proveedor proveedor)
         {
+         string token = Session["Token"].ToString();
             ProveedorManager manager = new ProveedorManager();
             await manager.Actualizar(proveedor, token);
             return RedirectToAction("Details", new { id = proveedor.Codigo, token = token });
         }
 
         // GET: Proveedor/Delete/5
-        public async Task<ActionResult> Delete(int id, string token)
+        public async Task<ActionResult> Delete(int id)
         {
+         string token = Session["Token"].ToString();
             ProveedorManager manager = new ProveedorManager();
             await manager.Eliminar(id, token);
             return RedirectToAction("Index", new { token = token });
