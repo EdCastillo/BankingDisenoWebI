3e88aedf6bdcb8de13e241173782dcfe0f4db2b0
srojas06 <57544567+srojas06@users.noreply.github.com>
2021-08-17 00:59:10 -0600

    Update CategoriaController

--- ViewsBanking/Controllers/CategoriaController.cs
+++ ViewsBanking/Controllers/CategoriaController.cs
@@ -12,18 +12,27 @@ namespace ViewsBanking.Controllers
     public class CategoriaController : Controller
     {
         // GET: Categoria
-        public async Task<ActionResult> Index(string token)
+        
+        private static CategoriaManager manager=new CategoriaManager();
+        public async Task<ActionResult> Index()
         {
+            string token=Session["Token"].ToString();
             CategoriaManager manager = new CategoriaManager();
             IEnumerable<Categoria> list = await manager.GetAll(token);
-            return View(list);
+            ServicioManager managerServicio = new ServicioManager();
+            IEnumerable<Servicio> listServicio= await managerServicio.GetAll(token);
+            
+            dynamic model = new ExpandoObject();
+            model.Categoria = list;
+            model.Servicio = listServicio;
+            return View(model);
         }
 
         // GET: Categoria/Details/5
-        public async Task<ActionResult> Details(int id, string token)
+        public async Task<ActionResult> Details(int id)
         {
-            CategoriaManager manager = new CategoriaManager();
-            Categoria categoria = await manager.GetByID(id, token);
+           string token = Session["Token"].ToString();
+            Categoria categoria =await manager.GetByID(id,token);
             return View(categoria);
         }
 
@@ -35,11 +44,11 @@ namespace ViewsBanking.Controllers
 
         // POST: Categoria/Create
         [HttpPost]
-        public async Task<ActionResult> Create(Categoria categoria, string token)
+        public async Task<ActionResult> Create(Categoria categoria)
         {
            
 
-
+                string token = Session["Token"].ToString();
                 CategoriaManager manager = new CategoriaManager();
                 await manager.Insertar(categoria, token);
                 return RedirectToAction("Index", new { token = token });
@@ -48,8 +57,9 @@ namespace ViewsBanking.Controllers
         }
 
         // GET: Categoria/Edit/5
-        public async Task<ActionResult> Edit(int id, string token)
+        public async Task<ActionResult> Edit(int id)
         {
+            string token = Session["Token"].ToString();
             CategoriaManager manager = new CategoriaManager();
             Categoria categoria = await manager.GetByID(id, token);
             return View(categoria);
@@ -57,9 +67,9 @@ namespace ViewsBanking.Controllers
 
         // POST: Categoria/Edit/5
         [HttpPost]
-        public async Task<ActionResult> Edit(Categoria categoria, string token)
+        public async Task<ActionResult> Edit(Categoria categoria)
         {
-
+            string token = Session["Token"].ToString();
             CategoriaManager manager = new CategoriaManager();
             await manager.Actualizar(categoria, token);
             return RedirectToAction("Details", new { id = categoria.Codigo, token = token });
@@ -67,8 +77,9 @@ namespace ViewsBanking.Controllers
         }
 
         // GET: Categoria/Delete/5
-        public async Task<ActionResult>Delete(int id, string token)
+        public async Task<ActionResult>Delete(int id)
         {
+        string token = Session["Token"].ToString();
             // TODO: Add delete logic here
             CategoriaManager manager = new CategoriaManager();
             await manager.Eliminar(id, token);
